b71892256b57c7b85e2ecdae30460be5b2ad46c8
InitSync <benjaalejandro206@gmail.com>
2022-09-14 20:57:52 -0300

    feat: implemented interface to use the ListenerHandler builder.

---
+++ plugin/src/main/java/team/aquatic/betterjoin/interfaces/ListenerHandler.java
@@ -0,0 +1,11 @@
+package team.aquatic.betterjoin.interfaces;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import team.aquatic.betterjoin.handlers.ListenerHandlerModel;
+
+public interface ListenerHandler {
+	static ListenerHandlerModel.Builder newEvent(@NotNull JavaPlugin plugin) {
+		return new ListenerHandlerModel.Builder(plugin);
+	}
+}
