37f04307f8b23e0f899deb987ccb4edac73e11df
InitSync <benjaalejandro206@gmail.com>
2022-09-15 12:44:42 -0300

    refactor: added new method (moved from ConfigManager), and renamed us to 'newInstance'.

--- plugin/src/main/java/team/aquatic/betterjoin/interfaces/ConfigHandler.java
+++ plugin/src/main/java/team/aquatic/betterjoin/interfaces/ConfigHandler.java
@@ -1,11 +1,16 @@
 package team.aquatic.betterjoin.interfaces;
 
 import org.jetbrains.annotations.NotNull;
+import team.aquatic.betterjoin.BetterJoin;
 import team.aquatic.betterjoin.enums.Configuration;
 import team.aquatic.betterjoin.managers.ConfigurationManager;
 
 public interface ConfigHandler {
-	static Configuration instance(@NotNull ConfigurationManager manager) {
+	static Configuration newInstance(@NotNull ConfigurationManager manager) {
 		return new Configuration(manager);
+	}
+	
+	static ConfigurationManager newInstance(@NotNull BetterJoin plugin, @NotNull String... files) {
+		return new ConfigurationManager(plugin, files);
 	}
 }
