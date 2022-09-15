8b74275ab7853caf670274463ff5ca1b5933cd8d
InitSync <benjaalejandro206@gmail.com>
2022-09-15 14:00:21 -0300

    chore: updated file.

--- plugin/src/main/java/team/aquatic/betterjoin/enums/modules/config/MessagesPathImpl.java
+++ plugin/src/main/java/team/aquatic/betterjoin/enums/modules/config/MessagesPathImpl.java
@@ -5,7 +5,15 @@ import org.jetbrains.annotations.NotNull;
 import team.aquatic.betterjoin.enums.Path;
 
 public enum MessagesPathImpl implements Path {
-	NO_PERM ("no-permission");
+	NO_PERM ("no-permission"),
+	NO_COMMAND ("no-command"),
+	NO_FILE ("no-file"),
+	
+	HELP ("help"),
+	
+	RELOAD_ALL ("reload-all"),
+	RELOAD_CONFIG ("reload-config"),
+	RELOAD_MESSAGES ("reload-messages");
 	
 	private final String path;
 	
