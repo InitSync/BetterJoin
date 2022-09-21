cd5cadc724c9daf90ae420ed3178425eb7e3674b
InitSync <benjaalejandro206@gmail.com>
2022-09-21 14:22:07 -0300

    refactor(action): simplified ActionHandler avoiding long methods and implemented small methods for each action.

--- plugin/src/main/java/team/aquatic/betterjoin/handlers/ActionFactoryHandler.java
+++ plugin/src/main/java/team/aquatic/betterjoin/handlers/ActionFactoryHandler.java
@@ -9,7 +9,6 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 import team.aquatic.betterjoin.BetterJoin;
 import team.aquatic.betterjoin.enums.modules.actions.ActionType;
@@ -52,193 +51,173 @@ public class ActionFactoryHandler {
 		
 		public void make() {
 			if (actionType == ActionType.SOUND) {
-				this.container = this.container.substring(8);
-				this.splitter = this.container.split(";", 3);
-				
-				final Optional<XSound> soundOptional = XSound.matchXSound(this.splitter[0]);
-				if (!soundOptional.isPresent()) {
-					LogPrinter.error("The sound for the action is not present at the container.");
-					return;
-				}
-				
-				final Sound bukkitSound = soundOptional.get().parseSound();
-				assert bukkitSound != null;
-				
-				int volume;
-				int pitch;
-				try {
-					volume = Integer.parseInt(this.splitter[1]);
-					pitch = Integer.parseInt(this.splitter[2]);
-				} catch (NumberFormatException exception) {
-					LogPrinter.error("Failed to parse the sound action parameters.");
-					
-					exception.printStackTrace();
-					return;
-				}
-				
-				player.playSound(
-					 player.getLocation(),
-					 bukkitSound,
-					 volume, pitch
-				);
+				this.executeSoundAction();
 				return;
 			}
 			
 			if (actionType == ActionType.EFFECT) {
-				this.container = this.container.substring(9);
-				this.splitter = this.container.split(";", 3);
-				
-				final Optional<XPotion> potionOptional = XPotion.matchXPotion(this.splitter[0]);
-				if (!potionOptional.isPresent()) {
-					LogPrinter.error("The effect for the action is not present at the container.");
-					return;
-				}
-				
-				final PotionEffectType effectType = potionOptional.get().getPotionEffectType();
-				assert effectType != null;
-				
-				int duration;
-				int amplifier;
-				try {
-					duration = Integer.parseInt(this.splitter[1]);
-					amplifier = Integer.parseInt(this.splitter[2]);
-				} catch (NumberFormatException exception) {
-					LogPrinter.error("Failed to parse the effect action parameters.");
-					
-					exception.printStackTrace();
-					return;
-				}
-				
-				player.addPotionEffect(new PotionEffect(effectType, duration, amplifier));
+				this.executeEffectAction();
 				return;
 			}
 			
 			if (actionType == ActionType.TITLE) {
-				this.container = this.container.substring(8);
-				this.splitter = container.split(";", 5);
-				
-				int fadeIn;
-				int stay;
-				int fadeOut;
-				try {
-					fadeIn = Integer.parseInt(this.splitter[2]);
-					stay = Integer.parseInt(this.splitter[3]);
-					fadeOut = Integer.parseInt(this.splitter[4]);
-				} catch (NumberFormatException exception) {
-					LogPrinter.error("Failed to parse the title action parameters.");
-					
-					exception.printStackTrace();
-					return;
-				}
-				
-				Utils.sendTitle(
-					 player,
-					 this.splitter[0],
-					 this.splitter[1],
-					 fadeIn,
-					 stay,
-					 fadeOut
-				);
+				this.executeTitleAction();
 				return;
 			}
 			
 			if (actionType == ActionType.ACTION_BAR) {
-				this.container = this.container.substring(12);
-				this.splitter = this.container.split(";", 2);
-				
-				Utils.sendActionBar(
-					 this.plugin,
-					 player,
-					 this.splitter[1],
-					 Long.parseLong(this.splitter[0])
-				);
-				return;
-			}
-			
-			if (actionType == ActionType.FIREWORK) {
+				this.executeActionBarAction();
 				return;
 			}
 			
 			if (actionType == ActionType.COMMAND) {
-				this.container = this.container.substring(10);
-				this.splitter = this.container.split(";", 2);
-				final String command = IridiumColorAPI.process(this.splitter[0]);
-				
-				if (Boolean.parseBoolean(this.splitter[1])) {
-					if (this.plugin
-						 .getServer()
-						 .isPrimaryThread()
-					) {
-						this.plugin
-							 .getServer()
-							 .dispatchCommand(
-									Bukkit.getConsoleSender(),
-									command
-							 );
-						return;
-					}
-					
-					new BukkitRunnable() {
-						@Override
-						public void run() {
-							plugin.getServer()
-								 .dispatchCommand(
-										Bukkit.getConsoleSender(),
-										command
-								 );
-							cancel();
-						}
-					}.runTaskLater(this.plugin, 1L);
-					return;
-				}
-				
-				if (this.plugin
-					 .getServer()
-					 .isPrimaryThread()
-				) {
-					this.plugin
-						 .getServer()
-						 .dispatchCommand(player, command);
-					return;
-				}
-				
-				new BukkitRunnable() {
-					@Override
-					public void run() {
-						plugin.getServer().dispatchCommand(player, command);
-						cancel();
-					}
-				}.runTaskLater(this.plugin, 1L);
+				this.executeCommandAction();
 				return;
 			}
 			
 			if (actionType == ActionType.BROADCAST) {
-				this.plugin
-					 .getServer()
-					 .getOnlinePlayers()
-					 .forEach(player1 -> {
-						 player.sendMessage(IridiumColorAPI.process(
-							  container.substring(12)
-						 ));
-					 });
+				this.executeBroadcastAction();
 				return;
 			}
 			
-			if (actionType == ActionType.MESSAGE) {
-				player.sendMessage(IridiumColorAPI.process(
-					 container.substring(10)
-				));
+			if (actionType == ActionType.MESSAGE) this.executeMessageAction();
+		}
+		
+		private void executeSoundAction() {
+			this.container = this.container.substring(8);
+			this.splitter = this.container.split(";", 3);
+			
+			final Optional<XSound> soundOptional = XSound.matchXSound(this.splitter[0]);
+			if (!soundOptional.isPresent()) {
+				LogPrinter.error("The sound for the action is not present at the container.");
 				return;
 			}
 			
-			if (actionType == ActionType.CONSOLE) {
+			final Sound bukkitSound = soundOptional.get().parseSound();
+			assert bukkitSound != null;
+			
+			int volume;
+			int pitch;
+			try {
+				volume = Integer.parseInt(this.splitter[1]);
+				pitch = Integer.parseInt(this.splitter[2]);
+			} catch (NumberFormatException exception) {
+				LogPrinter.error("Failed to parse the sound action parameters.");
+				
+				exception.printStackTrace();
+				return;
+			}
+			
+			player.playSound(
+				 player.getLocation(),
+				 bukkitSound,
+				 volume, pitch
+			);
+		}
+		
+		private void executeEffectAction() {
+			this.container = this.container.substring(9);
+			this.splitter = this.container.split(";", 3);
+			
+			final Optional<XPotion> potionOptional = XPotion.matchXPotion(this.splitter[0]);
+			if (!potionOptional.isPresent()) {
+				LogPrinter.error("The effect for the action is not present at the container.");
+				return;
+			}
+			
+			final PotionEffectType effectType = potionOptional.get().getPotionEffectType();
+			assert effectType != null;
+			
+			int duration;
+			int amplifier;
+			try {
+				duration = Integer.parseInt(this.splitter[1]);
+				amplifier = Integer.parseInt(this.splitter[2]);
+			} catch (NumberFormatException exception) {
+				LogPrinter.error("Failed to parse the effect action parameters.");
+				
+				exception.printStackTrace();
+				return;
+			}
+			
+			player.addPotionEffect(new PotionEffect(effectType, duration, amplifier));
+		}
+		
+		private void executeTitleAction() {
+			this.container = this.container.substring(8);
+			this.splitter = container.split(";", 5);
+			
+			int fadeIn;
+			int stay;
+			int fadeOut;
+			try {
+				fadeIn = Integer.parseInt(this.splitter[2]);
+				stay = Integer.parseInt(this.splitter[3]);
+				fadeOut = Integer.parseInt(this.splitter[4]);
+			} catch (NumberFormatException exception) {
+				LogPrinter.error("Failed to parse the title action parameters.");
+				
+				exception.printStackTrace();
+				return;
+			}
+			
+			Utils.sendTitle(
+				 player,
+				 this.splitter[0],
+				 this.splitter[1],
+				 fadeIn,
+				 stay,
+				 fadeOut
+			);
+		}
+		
+		private void executeActionBarAction() {
+			this.container = this.container.substring(12);
+			this.splitter = this.container.split(";", 2);
+			
+			Utils.sendActionBar(
+				 this.plugin,
+				 player,
+				 this.splitter[1],
+				 Long.parseLong(this.splitter[0])
+			);
+		}
+		
+		private void executeCommandAction() {
+			this.container = this.container.substring(10);
+			this.splitter = this.container.split(";", 2);
+			final String command = IridiumColorAPI.process(this.splitter[0]);
+			
+			if (Boolean.parseBoolean(this.splitter[1])) {
 				this.plugin
 					 .getServer()
-					 .getConsoleSender()
-					 .sendMessage(IridiumColorAPI.process(
-							container.substring(10)
+					 .dispatchCommand(
+							Bukkit.getConsoleSender(),
+							command
+					 );
+				return;
+			}
+			
+			this.plugin
+				 .getServer()
+				 .dispatchCommand(player, command);
+		}
+		
+		private void executeBroadcastAction() {
+			this.plugin
+				 .getServer()
+				 .getOnlinePlayers()
+				 .forEach(player1 -> {
+					 player.sendMessage(IridiumColorAPI.process(
+							container.substring(12)
 					 ));
-			}
+				 });
+		}
+		
+		private void executeMessageAction() {
+			player.sendMessage(IridiumColorAPI.process(
+				 container.substring(10)
+			));
 		}
 	}
 }
