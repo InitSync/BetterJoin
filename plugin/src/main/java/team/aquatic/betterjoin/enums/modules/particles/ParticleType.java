ad35fbcf3e877a4636dc34001e2b5a0aaecaa571
InitSync <benjaalejandro206@gmail.com>
2022-09-21 19:05:23 -0300

    feat(particle): implemented enum for the particle form types

---
+++ plugin/src/main/java/team/aquatic/betterjoin/enums/modules/particles/ParticleType.java
@@ -0,0 +1,14 @@
+package team.aquatic.betterjoin.enums.modules.particles;
+
+public enum ParticleType {
+	CIRCLE,
+	CLOUD,
+	ATOM,
+	CONE,
+	DIAMOND,
+	LINE,
+	EYE,
+	EXPLOSION,
+	HEART,
+	POLYGON
+}
