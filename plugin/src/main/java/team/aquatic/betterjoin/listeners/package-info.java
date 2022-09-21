b6561f6d70251cdc4d8a562476bdd133624c3afc
InitSync <benjaalejandro206@gmail.com>
2022-09-21 19:25:51 -0300

    feat(info): added package-info

---
+++ plugin/src/main/java/team/aquatic/betterjoin/listeners/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that contains the plugin listeners.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author InitSync
+ */
+package team.aquatic.betterjoin.listeners;
---
+++ plugin/src/main/java/team/aquatic/betterjoin/particles/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that contains the particles classes and their implementation.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author InitSync
+ */
+package team.aquatic.betterjoin.particles;
