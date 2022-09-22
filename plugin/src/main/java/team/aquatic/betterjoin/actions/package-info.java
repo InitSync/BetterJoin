4f5e783badf23a848ff454cae2496b00782d391b
InitSync <benjaalejandro206@gmail.com>
2022-09-22 00:06:19 -0300

    feat(info): added package-info

---
+++ plugin/src/main/java/team/aquatic/betterjoin/actions/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * At this package you can find the ActionExecutable abstract class and their implementations.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author InitSync
+ */
+package team.aquatic.betterjoin.actions;
