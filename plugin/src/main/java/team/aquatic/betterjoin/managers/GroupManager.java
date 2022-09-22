738a3b2e175b32fdae29bc91a68a01aeed21dac8
InitSync <benjaalejandro206@gmail.com>
2022-09-22 00:16:29 -0300

    feat(manager): added 'executeGroupParticles()' method

--- plugin/src/main/java/team/aquatic/betterjoin/managers/GroupManager.java
+++ plugin/src/main/java/team/aquatic/betterjoin/managers/GroupManager.java
@@ -5,10 +5,9 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import team.aquatic.betterjoin.BetterJoin;
+import team.aquatic.betterjoin.api.ParticleShowEvent;
 import team.aquatic.betterjoin.enums.Configuration;
-import team.aquatic.betterjoin.enums.modules.actions.ActionType;
 import team.aquatic.betterjoin.enums.modules.files.FileType;
-import team.aquatic.betterjoin.interfaces.ActionInterface;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -17,11 +16,13 @@ public class GroupManager {
 	private final BetterJoin plugin;
 	private final Configuration configuration;
 	private final LuckPerms luckPerms;
+	private final ParticleManager particleManager;
 	
 	public GroupManager(@NotNull BetterJoin plugin) {
 		this.plugin = Objects.requireNonNull(plugin, "BetterJoin instance is null.");
 		this.configuration = this.plugin.configuration();
 		this.luckPerms = this.plugin.luckPerms();
+		this.particleManager = this.plugin.particleManager();
 	}
 	
 	public @Nullable String getPlayerGroup(@NotNull UUID uuid) {
@@ -72,16 +73,41 @@ public class GroupManager {
 		
 		final String group = this.getPlayerGroup(player.getUniqueId());
 		if (this.configuration.section(FileType.CONFIG, "config.server.groups." + group) != null) {
-			this.configuration
-				 .stringList(FileType.CONFIG, "config.server.groups." + group + ".actions")
-				 .forEach(action -> {
-					 final String actionPrefix = action.split(";")[0];
-					 ActionInterface.newAction(this.plugin)
-							.toPlayer(player)
-							.action(ActionType.valueOf(actionPrefix))
-							.container(action.replace(actionPrefix, ""))
-							.make();
-				 });
+			this.plugin
+				 .actionManager()
+				 .executeActions(
+						player,
+					  this.configuration.stringList(
+							 FileType.CONFIG,
+						   "config.server.groups." + group + ".actions"
+					  )
+				 );
+		}
+	}
+	
+	public void executeGroupParticles(@NotNull Player player) {
+		Objects.requireNonNull(player, "The player is null.");
+		
+		final ParticleShowEvent particleShowEvent = new ParticleShowEvent(
+			 player,
+			 this.particleManager.userParticleType(player.getUniqueId()).name()
+		);
+		this.plugin
+			 .getServer()
+			 .getPluginManager()
+			 .callEvent(particleShowEvent);
+		if (!particleShowEvent.isCancelled()) {
+			final String group = this.getPlayerGroup(player.getUniqueId());
+			if (this.configuration.section(FileType.CONFIG, "config.server.groups." + group) != null) {
+				this.particleManager
+					 .showForm(
+							player,
+							this.particleManager.userParticleType(player.getUniqueId()),
+							this.configuration.string(
+								 FileType.CONFIG,
+								 "config.server.groups." + group + ".particle-params")
+					 );
+			}
 		}
 	}
 }
