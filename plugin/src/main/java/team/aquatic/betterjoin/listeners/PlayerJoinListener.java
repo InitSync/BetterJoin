28fb2b252c24f0729dcdfecbc817aa74f6cac1d0
InitSync <benjaalejandro206@gmail.com>
2022-09-22 00:15:19 -0300

    rm(listener): removed 'executeGroupParticles()' class method

--- plugin/src/main/java/team/aquatic/betterjoin/listeners/PlayerJoinListener.java
+++ plugin/src/main/java/team/aquatic/betterjoin/listeners/PlayerJoinListener.java
@@ -8,12 +8,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 import team.aquatic.betterjoin.BetterJoin;
-import team.aquatic.betterjoin.api.ParticleShowEvent;
 import team.aquatic.betterjoin.api.UserServerJoinEvent;
 import team.aquatic.betterjoin.enums.Configuration;
 import team.aquatic.betterjoin.enums.modules.files.FileType;
 import team.aquatic.betterjoin.managers.GroupManager;
-import team.aquatic.betterjoin.managers.ParticleManager;
 import team.aquatic.betterjoin.utils.Utils;
 
 import java.util.Objects;
@@ -23,23 +21,17 @@ public class PlayerJoinListener implements Listener {
 	private final BetterJoin plugin;
 	private final Configuration configuration;
 	private final GroupManager groupManager;
-	private final ParticleManager particleManager;
-
-	private String group;
 	
 	public PlayerJoinListener(@NotNull BetterJoin plugin) {
 		this.plugin = Objects.requireNonNull(plugin, "BetterJoin instance is null.");
 		this.configuration = this.plugin.configuration();
 		this.groupManager = this.plugin.groupManager();
-		this.particleManager = this.plugin.particleManager();
 	}
 	
 	@EventHandler (priority = EventPriority.LOW)
 	public void onPlayerJoin(@NotNull PlayerJoinEvent event) {
 		final Player player = event.getPlayer();
 		final UUID uuid = player.getUniqueId();
-		
-		this.group = this.groupManager.getPlayerGroup(uuid);
 		
 		this.sendMotd(player);
 		
@@ -53,8 +45,7 @@ public class PlayerJoinListener implements Listener {
 			event.setJoinMessage(Utils.parse(player, serverJoinEvent.getJoinMessage()));
 			
 			this.groupManager.executeGroupActions(player);
-			
-			this.executeGroupParticles(player);
+			this.groupManager.executeGroupParticles(player);
 		}
 	}
 	
@@ -68,33 +59,5 @@ public class PlayerJoinListener implements Listener {
 		this.configuration
 			 .stringList(FileType.CONFIG, "config.server.motd")
 			 .forEach(string -> player.sendMessage(IridiumColorAPI.process(string)));
-	}
-	
-	private void executeGroupParticles(@NotNull Player player) {
-		final ParticleShowEvent particleShowEvent = new ParticleShowEvent(
-			 player,
-			 this.particleManager
-					.userParticleType(player.getUniqueId())
-					.name()
-		);
-		this.plugin
-			 .getServer()
-			 .getPluginManager()
-			 .callEvent(particleShowEvent);
-		if (!particleShowEvent.isCancelled()) {
-			if (this.configuration.check(
-				 FileType.CONFIG,
-				 "config.server.groups." + group + ".allow-particles")
-			) {
-				this.particleManager
-					 .showForm(
-							player,
-							this.particleManager.userParticleType(player.getUniqueId()),
-							this.configuration.string(
-								 FileType.CONFIG,
-								 "config.server.groups." + group + ".particle-params")
-					 );
-			}
-		}
 	}
 }
